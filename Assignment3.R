26639b599e0a9115e6c59922a12c8361b4b0fe51
yuoonnu <yuoonnu@gmail.com>
2025-11-13 16:22:45 +0900

    Update Assignment3.R

--- Assignment3.R
+++ Assignment3.R
@@ -1,18 +1,18 @@
-#1. 원그래프 그리기
+##원그래프 그리기
 #파일 불러오기
 death_data <- read.csv("C:/Downloads/사망원인_월별_사망자수.csv", fileEncoding = "EUC-KR")
 
 #원그래프 그리기
 pie(death_data$사망자수, labels=death_data$사망원인, main="2023년 12월 주요 사망원인", cex=0.8)
 
-#2. 선그래프 그리기
+##선그래프 그리기
 #파일 불러오기
 doctor_data <- read.csv("C:/Downloads/의료서비스_이용률.csv", fileEncoding="EUC-KR")
 
 #선그래프 그리기
 plot(doctor_data$시점, doctor_data$전체, col="purple", type="o", ylim=c(50, 70), ylab="이용자수")
 
-#3. 누적막대그래프 그리기
+##누적막대그래프 그리기
 #과학적 표기법으로 표시하지 않도록 설정
 options(scipen=999)
 
